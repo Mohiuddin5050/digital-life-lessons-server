217a3b46a11744a1651ecc87ec1e23160ed66613
Mohiuddin5050 <kmmohiuddin5050@gmail.com>
2025-12-16 06:33:20 +0600

    create users post api

--- index.js
+++ index.js
@@ -10,7 +10,57 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cors());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uji33wc.mongodb.net/?appName=Cluster0`;
 
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const db = client.db("digital_life_lessons");
+    const userCollection = db.collection("users");
+
+    // user api
+    app.get("/users", async (req, res) => {});
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      user.createdAt = new Date();
+      user.isPremium = false;
+      user.role = "user";
+
+      //check user already exit or not
+      const email = user.email;
+      const userExits = await userCollection.findOne({ email });
+
+      if (userExits) {
+        return res.send({ message: "User already exits" });
+      }
+
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
